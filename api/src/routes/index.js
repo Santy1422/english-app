da907c5057172f18f163b8ab14ac2d5d423e36b2
Santy1422 <102694038+Santy1422@users.noreply.github.com>
2023-02-24 23:30:02 +0100

    saf

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,6 +5,6 @@ const ingles = require(('./ingles.route.js'))
 
 const router = express.Router();
 
-router.use('/ingles', cors({ origin: 'https://english-app-plum.vercel.app' }), ingles);
+router.use('/ingles', cors({ origin: '*' }), ingles);
 
 module.exports = router;
