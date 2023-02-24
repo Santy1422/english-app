039db7d8b9715a3dee5c228a1aab1f1380d9af5b
Santy1422 <102694038+Santy1422@users.noreply.github.com>
2023-02-24 23:16:07 +0100

    asf

--- api/src/server.js
+++ api/src/server.js
@@ -6,49 +6,52 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const { checkJwt, firebaseAdmin, setAdmin, extractUserData } = require('./utils/firebase-stuff');
 const {globalLimit} = require('./utils/rate-limiters');
-// permite leer archivo .env.
-require('dotenv').config();
 
+// permite leer archivo .env
+require('dotenv').config();
 
 //#region  MONGOOSE  
 mongoose.set('strictQuery', true)
-const DATABASE_URL = process.env.DATABASE_URL ? process.env.DATABASE_URL : 'mongodb://localhost:27017';
-const DATABASE_NAME = process.env.DATABASE_NAME || 'findahome';
-async function main() {//conecta mongoose a la database
-         await mongoose.connect(DATABASE_URL+"/"+DATABASE_NAME);
-// use  `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+
+// Agrega aquí las variables de entorno necesarias para conectarte a tu base de datos de MongoDB
+const DATABASE_URL = 'mongodb://' + process.env.DATABASE_USER + ':' + process.env.DATABASE_PASSWORD + '@' + process.env.DATABASE_HOST + ':' + process.env.DATABASE_PORT + '/' + process.env.DATABASE_NAME;
+
+async function main() {
+  // Conecta mongoose a la database
+  await mongoose.connect(DATABASE_URL);
 }
 
-main().catch(err => console.log({error: 'Error al conectar con la database' + err.message})); //como main es asincronica, es una promesa, tiene .catch:
+main().catch(err => console.log({error: 'Error al conectar con la database' + err.message}));
+
 //#endregion
 
-
 const create = async () => {
-    //crea el server de express, no lo inicia todavía, tiene q iniciarl luego de conectar a la database
-    const app = express();
-    //MIDDLEWARES, se meten en todos los request y en todos los sends
-    app.use(cors()) //discrimina quien puede hacer peticiones al backend, poner pagina del frontend al deployar.
-    app.use(globalLimit)
-    app.use(express.json({ limit: "50mb" })) //transforma json en strings automaticamente y viceversa.
-    app.use(bodyParser.urlencoded({ extended: true })); //permite anidacion de objetos y arrays
-    app.use(express.static('public')); //no recuerdo
-    //FIN MIDDLEWARES
-    //Todas las Rutas:
-    app.use(routes)
+  // Crea el servidor de express, no lo inicia todavía, tiene que iniciarlo luego de conectar a la base de datos
+  const app = express();
+  
+  // MIDDLEWARES, se meten en todos los request y en todos los sends
+  app.use(cors()); // Discrimina quién puede hacer peticiones al backend, poner página del frontend al deployar.
+  app.use(globalLimit);
+  app.use(express.json({ limit: "50mb" })); // Transforma json en strings automáticamente y viceversa.
+  app.use(bodyParser.urlencoded({ extended: true })); // Permite anidación de objetos y arrays
+  app.use(express.static('public')); // No recuerdo
+  // FIN MIDDLEWARES
+  
+  // Todas las Rutas:
+  app.use(routes);
 
-    app.get('/check', checkJwt, async (req, res) => {
-        //setAdmin(req.user.uid)
-        try{
-            res.send({message: 'Token decodificado exitosamente!', user: req.user})
+  app.get('/check', checkJwt, async (req, res) => {
+    //setAdmin(req.user.uid)
+    try {
+      res.send({ message: 'Token decodificado exitosamente!', user: req.user });
+    } catch (err) {
+      res.send({ message: 'el back exploto' + err.message });
+    }
+  });
 
-        }catch(err){
-            res.send({message: 'el back exploto' + err.message})
-        }
-    });
-
-    return app
-}
+  return app;
+};
 
 module.exports = {
-    create
+  create
 };
