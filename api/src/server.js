46c7a8d4618c64b35d1071f5b68995cbd3f90796
Santy1422 <102694038+Santy1422@users.noreply.github.com>
2023-02-24 23:41:17 +0100

    fsaf

--- api/src/server.js
+++ api/src/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+// const cors = require('cors');
 const { checkJwt, firebaseAdmin, setAdmin, extractUserData } = require('./utils/firebase-stuff');
 const {globalLimit} = require('./utils/rate-limiters');
 
@@ -30,7 +30,10 @@ const create = async () => {
   const app = express();
   
   // MIDDLEWARES, se meten en todos los request y en todos los sends
-  app.use(cors({ origin: '*' })); // Discrimina quién puede hacer peticiones al backend, poner página del frontend al deployar.
+  app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(cookieParser());
+  app.use(morgan('dev'));
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
     // res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
@@ -39,10 +42,6 @@ const create = async () => {
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
   });
-  app.use(globalLimit);
-  app.use(express.json({ limit: "50mb" })); // Transforma json en strings automáticamente y viceversa.
-  app.use(bodyParser.urlencoded({ extended: true })); // Permite anidación de objetos y arrays
-  app.use(express.static('public')); // No recuerdo
   // FIN MIDDLEWARES
   
   // Todas las Rutas:
