f0dcae5ba9755760c0e57a70c11d2b9d1a47e50f
Santy1422 <102694038+Santy1422@users.noreply.github.com>
2023-02-25 20:41:10 +0100

    fix delete

--- Client/src/Components/useCard.jsx
+++ Client/src/Components/useCard.jsx
@@ -28,14 +28,14 @@ const prev = () => {
 const deleteWord = async () =>{
 
     try{
-let word = await axios.put("/ingles/delete",
+await axios.put("/ingles/delete",
  
 {    email: profile.email,
       palabra: profile.palabras.español[posicion],
       word: profile.palabras.ingles[posicion],
       image: profile.palabras.image[posicion]
 } )
-.then((succces) => alert ("Palabra marcada como aprendida!"), window.location.reload() )
+.then((succces) => alert ("Palabra marcada como aprendida!") )
 
 }
 catch(err) {
