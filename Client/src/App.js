493e597468520fe5ad0eb0f0758fa449259aecca
Santiago Garcia <102694038+Santy1422@users.noreply.github.com>
2023-05-09 11:29:12 +0200

    deploy active

--- Client/src/App.js
+++ Client/src/App.js
@@ -5,8 +5,8 @@ import { Home } from './Components/Home';
 import { Panel } from './Components/Panel';
 import axios from "axios"
 
-// axios.defaults.baseURL = "https://english-app-production.up.railway.app"
-axios.defaults.baseURL = "http://localhost:8080"
+axios.defaults.baseURL = "https://english-app-production.up.railway.app"
+// axios.defaults.baseURL = "http://localhost:8080"
 
 // axios.defaults.withCredentials = true;
 
